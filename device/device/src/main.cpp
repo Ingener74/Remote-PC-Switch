19362cbb2570fddc7e9ae9ec98bd51925ba1e7d0
Павел Шнайдер <shnaider-p@playrix.com>
2019-03-07 09:25:00 +0500

    Запустил подключение к WiFi на устройстве

--- device/device/src/main.cpp
+++ device/device/src/main.cpp
@@ -1,7 +1,32 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+const char* SSID = "iPhone";
+const char* PW = "f0755vhxgasa9";
 
 void setup() {
+    Serial.begin(115200);
+
+    Serial.println();Serial.println();Serial.println();
+    Serial.println("Start");
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(SSID, PW);
+
+    while(WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+
+    Serial.println("WiFi connected");
+    Serial.print("IP address: "); Serial.println(WiFi.localIP());
 }
 
+const char* wheel = "|/-\\";
 void loop() {
+    for (int i = 0; i < 4; ++i) {
+        Serial.print(wheel[i]); Serial.print("\r");
+        delay(1000);
+    }
 }
