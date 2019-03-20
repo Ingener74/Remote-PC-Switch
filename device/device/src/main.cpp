5d416184812f395472e5e01cb6b3bb7dcbe76a0e
Shnaider Pavel <shnaiderpasha@gmail.com>
2019-03-20 16:02:36 +0500

    Добавил запрос состояния и приём Json с ответом что делать

--- device/device/src/main.cpp
+++ device/device/src/main.cpp
@@ -4,7 +4,7 @@
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
 
-// #include <Keyboard.h>
+#include <ArduinoJson.h>
 
 const char* SSID = "iPhone";
 const char* PW = "f0755vhxgasa9";
@@ -20,8 +20,6 @@ void setup() {
 
     WiFi.mode(WIFI_STA);
     WiFiMulti.addAP(SSID, PW);
-
-    // Keyboard.begin();
 }
 
 void loop() {
@@ -31,13 +29,30 @@ void loop() {
         Serial.printf("[WiFi] local address: %s\r", client.localIP().toString().c_str());
         delay(1000);
         Serial.printf("[HTTP] begin...%*s\r", 40, " ");
-        if (http.begin(client, "http://172.20.10.4:5000/")) {
+        if (http.begin(client, "http://172.20.10.4:5000/esp8266/QUERY")) {
             int httpCode = http.GET();
 
             if (httpCode > 0) {
                 if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
                     String payload = http.getString();
-                    Serial.printf("[HTTP] received: %s\n", payload.c_str());
+                    
+                    // Serial.printf("[HTTP] received: %s\n", payload.c_str());
+                    
+                    StaticJsonDocument<256> doc;
+                    
+                    DeserializationError error = deserializeJson(doc, payload);
+
+                    // Test if parsing succeeds.
+                    if (error) {
+                        Serial.printf("deserializeJson() failed: %s\r", error.c_str());
+                        delay(1000);
+                        return;
+                    } else {
+                        const char* act = doc["act"];
+                        Serial.printf("Received act: %s%*s\r", act, 40, " ");
+
+                        delay(2000);
+                    }
                 }
             } else {
                 Serial.printf("[HTTP] error: %s\r", http.errorToString(httpCode).c_str());
